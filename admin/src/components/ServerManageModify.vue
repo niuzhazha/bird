526e3d5303d49d5455a95aac0b41afe632a293c7
Liu Jiahuan <liujiahuan@xuetangx.com>
2017-09-21 09:46:56 +0800

    modify 验证

--- admin/src/components/ServerManageModify.vue
+++ admin/src/components/ServerManageModify.vue
@@ -1,6 +1,177 @@
 <template>
-  <!-- :rules="ruleValidate" -->
-  <el-form class="modify" :rules="rules" ref="form" :model="server" 
+  <el-form 
+    :model="formInline" 
+    :rules="ruleInline" 
+    ref="formInline"
+    label-width="110px" style="background:#fff;padding: 20px;"
+    class="login-ruleForm">
+    
+    <div class="title-oprate pb14 bor-btm-ec">
+      <span class="font14 col9">
+        <router-link v-if="" :to="{ path: '/lnav-1' }" class="back-icon">
+          <img src="../assets/images/backIcon.png">
+        </router-link>
+        添加/修改服务商信息
+      </span>
+    </div>
+
+    <div class="server-count font14 col9">基本信息</div>
+    <el-form-item label="公司全称：" prop="companyName">
+      <el-row>
+        <el-col :span="10">
+          <el-input v-model="formInline.companyName" placeholder="请输入公司全称，和营业执照上的保持一致"></el-input>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    <el-form-item label="公司简称：" prop="companyNameShort">
+      <el-row>
+        <el-col :span="10">
+          <el-input v-model="formInline.companyNameShort" placeholder="请输入公司简介"></el-input>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    
+    <el-form-item label="公司LOGO：" required prop="logo_file">
+      <el-row>
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :on-remove="handleRemove_logo"
+          :on-success="uploadSuccess_logo"
+          :before-upload="beforeUpload_logo">
+          <el-button>点击上传公司LOGO</el-button>
+        </el-upload>
+      </el-row>
+    </el-form-item>
+
+    <el-form-item label="公司规模：" prop="scale">
+      <el-row>
+        <el-col :span="10">
+          <el-select v-model="formInline.scale" placeholder="请选择公司规模">
+            <el-option
+              v-for="scale in scales"
+              :key="scale.code"
+              :label="scale.name"
+              :value="scale.code">
+            </el-option>
+          </el-select>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    
+    <el-form-item label="所在城市：" required>
+      <el-row>
+        <el-col :span="6">
+          <el-form-item prop="province">
+            <el-select v-model="formInline.province" placeholder="请选择" @change="select">
+              <el-option
+                v-for="province in citys"
+                :key="province.code"
+                :label="province.name"
+                :value="province.code">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+        <el-col :span="6" style="margin-left: 20px;">
+          <el-form-item prop="city">
+            <el-select v-model="formInline.city" placeholder="请选择">
+              <el-option
+                v-for="city in subcitys"
+                :key="city.code"
+                :label="city.name"
+                :value="city.code">
+              </el-option>
+            </el-select>
+          </el-form-item>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    
+    <el-form-item label="公司地址：" prop="addr">
+      <el-row>
+        <el-col :span="10">
+          <el-input v-model="formInline.addr"></el-input>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    <el-form-item label="公司法人：" prop="legalPerson">
+      <el-row>
+        <el-col :span="10">
+          <el-input v-model="formInline.legalPerson"></el-input>
+        </el-col>
+      </el-row>
+    </el-form-item>
+    
+    <el-form-item label="公司简介：" prop="description">
+      <el-row>
+        <el-col :span="10">
+          <el-input type="textarea" 
+            v-model="formInline.description" 
+            placeholder="请输入公司简介..."></el-input>
+        </el-col>
+      </el-row>
+    </el-form-item>
+
+    <el-form-item label="营业执照：" required prop="licenseList">
+      <el-row>
+        <!-- <img :src="item" v-for="item in server.licenseList"> -->
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :on-remove="handleRemove_license"
+          :on-success="uploadSuccess_license">
+          <el-button>点击上传营业执照</el-button>
+        </el-upload>
+      </el-row>
+    </el-form-item>
+    
+    <el-form-item label="合同附件：" required prop="contractList">
+      <el-row>
+        <!-- <img :src="item" v-for="item in server.contractList"> -->
+        <el-upload
+          action="https://jsonplaceholder.typicode.com/posts/"
+          list-type="picture-card"
+          :on-remove="handleRemove_contract"
+          :on-success="uploadSuccess_contract">
+          <el-button>点击上传合同附件</el-button>
+        </el-upload>
+      </el-row>
+    </el-form-item>
+
+    <div class="server-count font14 col9">费用信息</div>
+    <el-form-item label="技术使用费：" prop="useRate_input">
+      <el-radio-group v-model="formInline.useRate_radio">
+        <el-radio class="radio" :label="1">1.2%</el-radio>
+        <el-radio class="radio" :label="2">
+          <el-input v-model="formInline.useRate_input" 
+            placeholder="请输入费率" :disabled="formInline.useRate_radio === 1"></el-input>
+          <span style="padding-left:10px;">%</span>
+        </el-radio>
+      </el-radio-group>
+    </el-form-item>
+    
+    <el-form-item label="个人税率：" prop="taxRate_input">
+      <el-radio-group v-model="formInline.taxRate_radio">
+        <el-radio class="radio" :label="1">1.2%</el-radio>
+        <el-radio class="radio" :label="2">
+          <el-input v-model="formInline.taxRate_input" 
+            placeholder="请输入费率" :disabled="formInline.taxRate_radio === 1"></el-input>
+          <span style="padding-left:10px;">%</span>
+        </el-radio>
+      </el-radio-group>
+    </el-form-item>
+
+    <el-form-item class="submit-item tc">
+      <el-button class="next-btn" type="primary" @click="onSubmit('formInline')">下一步</el-button>
+      <!-- <button class="next-btn" type="primary" @click="onSubmit('form')">下一步</button> -->
+    </el-form-item>
+  </el-form>
+  <!-- <el-form 
+    class="modify"
+    :rules="rules"
+    ref="form"
+    :model="server" 
     label-width="110px" style="background:#fff;padding: 20px;"
     v-on:submit.prevent>
     <div class="title-oprate pb14 bor-btm-ec">
@@ -27,9 +198,6 @@
           :on-remove="handleRemove">
           <el-button>点击上传公司LOGO</el-button>
         </el-upload>
-        <!-- <el-dialog v-model="dialogVisible" size="tiny">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog> -->
       </el-row>
     </el-form-item>
     <el-form-item class="" label="公司规模：" prop="scale">
@@ -71,7 +239,6 @@
       <el-input v-model="server.legalPerson"></el-input>
     </el-form-item>
     <el-form-item label="公司简介：" prop="description">
-      <!-- <el-input v-model="form.intro"></el-input> -->
       <el-input type="textarea" v-model="server.description" placeholder="请输入公司简介..."></el-input>
     </el-form-item>
     <el-form-item label="营业执照：" prop="licenseList">
@@ -84,9 +251,6 @@
           :on-remove="handleRemove">
           <el-button>点击上传营业执照</el-button>
         </el-upload>
-        <!-- <el-dialog v-model="dialogVisible" size="tiny">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog> -->
       </el-row>
     </el-form-item>
     <el-form-item label="合同附件：" prop="contractList">
@@ -99,9 +263,6 @@
           :on-remove="handleRemove">
           <el-button>点击上传合同附件</el-button>
         </el-upload>
-        <!-- <el-dialog v-model="dialogVisible" size="tiny">
-          <img width="100%" :src="dialogImageUrl" alt="">
-        </el-dialog> -->
       </el-row>
     </el-form-item>
     <div class="server-count font14 col9">费用信息</div>
@@ -124,37 +285,75 @@
     <el-form-item>
       <button class="next-btn" type="primary" @click="onSubmit('form')">下一步</button>
     </el-form-item>
-  </el-form>
+  </el-form> -->
 </template>
 <script>
   export default {
     data () {
+      let useRatePass = (rule, value, callback) => {
+        if (this.formInline.useRate_radio === 2) {
+          if (value === '') {
+            callback(new Error('请输入费率'))
+          }
+        }
+      }
+      let taxRatePass = (rule, value, callback) => {
+        if (this.formInline.taxRate_radio === 2) {
+          if (value === '') {
+            callback(new Error('请输入费率'))
+          }
+        }
+      }
+      let logoPass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请输入公司LOGO'))
+        }
+      }
+      let licensePass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请上传公司营业执照'))
+        }
+      }
+      let contractPass = (rule, value, callback) => {
+        if (value === '') {
+          callback(new Error('请上传公司合同附件'))
+        }
+      }
       return {
-        server: {
-          useRate: {
-            radio: '',
-            input: ''
-          },
-          taxRate: {
-            radio: '',
-            input: ''
-          },
-          scale: ''
+        formInline: {
+          companyName: '',
+          companyNameShort: '',
+          logo_file: '',
+          scale: '',
+          province: '',
+          city: '',
+          addr: '',
+          legalPerson: '',
+          description: '',
+          licenseList: '',
+          contractList: '',
+          useRate_radio: 1,
+          useRate_input: '',
+          taxRate_radio: 1,
+          taxRate_input: ''
         },
-        rules: {
+        ruleInline: {
           companyName: [
             { required: true, message: '请输入公司全称', trigger: 'blur' }
           ],
           companyNameShort: [
             { required: true, message: '请输入公司简称', trigger: 'blur' }
           ],
-          logo: [
-            { required: true, message: '请输入公司LOGO', trigger: 'blur' }
+          logo_file: [
+            { validator: logoPass, trigger: 'blur' }
           ],
           scale: [
             { required: true, message: '请选择公司规模', trigger: 'blur' }
           ],
           province: [
+            { required: true, message: '请选择所在省份', trigger: 'blur' }
+          ],
+          city: [
             { required: true, message: '请选择所在城市', trigger: 'blur' }
           ],
           addr: [
@@ -167,45 +366,24 @@
             { required: true, message: '请输入公司简介', trigger: 'blur' }
           ],
           licenseList: [
-            { required: true, message: '请上传营业执照', trigger: 'blur' }
+            { validator: licensePass, trigger: 'blur' }
+          ],
+          contractList: [
+            { validator: contractPass, trigger: 'blur' }
+          ],
+          useRate_input: [
+            { validator: useRatePass, trigger: 'blur' }
+          ],
+          taxRate_input: [
+            { validator: taxRatePass, trigger: 'blur' }
           ]
-          // useRate: [
-          //   { required: true, message: '请选择技术使用费', trigger: 'blur' }
-          // ],
-          // taxRate: [
-          //   { required: true, message: '请选择个人税率', trigger: 'blur' }
-          // ]
-        },
-        server_box: {
-          province: '',
-          city: ''
         },
         citys: [],
         subcitys: [],
         scales: []
       }
     },
-    // watch: {
-    //   'server.province' (val, oldVal) {
-    //     console.log(val)
-    //     清空第二个选择框中的可选数据
-    //     this.$set(this.$data, 'subcitys', [])
-    //     清空第二个选择框中的数据
-    //     this.$set(this.server, 'city', '')
-    //     this.citys.forEach(item => {
-    //       // console.log(item.code)
-    //       // console.log(val)
-    //       省份的code === 你选择的code
-    //       if (item.code === val) {
-    //         console.log(item)
-    //         被选中的城市存下来
-    //         this.$set(this.$data, 'subcitys', item.cities)
-    //       }
-    //     })
-    //   }
-    // },
     mounted () {
-      console.log(this.$route)
       if (this.$route.params.type === 'edit') {
         let item = {
           id: 0,
@@ -244,26 +422,21 @@
       onSubmit (name) {
         this.$refs[name].validate((valid) => {
           if (valid) {
-            alert('submit!')
             console.log(valid)
-            localStorage.setItem('serverString', JSON.stringify(this.server))
+            // localStorage.setItem('serverString', JSON.stringify(this.server))
             // this.$router.push({
             //   name: 'ServerManageModifyTwo',
             //   params: { type: this.$route.params.type, id: this.$route.params.id }
             // })
           } else {
-            alert('error submit!!')
-            // return false
+            this.$message.error('表单验证失败!')
           }
         })
-        // console.log('submit!')
-        // console.log(this.server)
       },
       getCities () {
         let _this = this
         let cityApi = '/api/common/city'
         this.$http.get(cityApi).then(function (response) {
-          // console.log(response)
           _this.$set(_this.$data, 'citys', response.data.data)
         }).catch(function (error) {
           console.log(error)
@@ -281,7 +454,7 @@
       },
       select (val) {
         this.$set(this.$data, 'subcitys', [])
-        this.$set(this.server, 'city', '')
+        this.$set(this.formInline, 'city', '')
         this.citys.forEach(item => {
           // console.log(item.code)
           // console.log(val)
@@ -291,8 +464,38 @@
           }
         })
       },
-      handlePictureCardPreview () {},
-      handleRemove () {}
+      uploadSuccess_logo (response, file, fileList) {
+        this.formInline.logo_file = file.url
+      },
+      handleRemove_logo (file, fileList) {
+        this.formInline.logo_file = ''
+      },
+      beforeUpload_logo (file) {
+        if (this.formInline.logo_file) {
+          this.$message('只能上传一张LOGO图片')
+          return false
+        }
+      },
+      uploadSuccess_license (response, file, fileList) {
+        let urlArr = []
+        fileList.forEach(item => {
+          urlArr.push(item.url)
+        })
+        this.formInline.licenseList = urlArr.join()
+      },
+      handleRemove_license (file, fileList) {
+        this.formInline.licenseList = ''
+      },
+      uploadSuccess_contract (response, file, fileList) {
+        let urlArr = []
+        fileList.forEach(item => {
+          urlArr.push(item.url)
+        })
+        this.formInline.contractList = urlArr.join()
+      },
+      handleRemove_contract (file, fileList) {
+        this.formInline.contractList = ''
+      }
     }
   }
 </script>
@@ -303,14 +506,6 @@
   .pb14 {padding-bottom: 14px;}
   .bor-btm-ec {border-bottom: 1px solid #ececec;}
   .server-count {padding: 20px 0;}
-  .el-input {/*width: 290px;border: 1px solid #dcdcdc;*/}
-  /*.modify .el-input__inner {border: 1px solid #dcdcdc;width: 400px;}*/
-  /*.modify .el-select .el-input__inner {border: 1px solid #dcdcdc;width: 200px;}*/
-  /*.modify .width400 .el-input__inner {border: 1px solid #dcdcdc;width: 400px;}*/
-  /*.modify .el-form-item__label {text-align: left;}*/
-  .el-form-item__content {
-    width: 50%;
-  }
   .modify .el-button {
     font-size: 12px;
     height: 24px;
@@ -332,7 +527,7 @@
   .city .el-input:first-child .el-input__inner {margin-right: 20px;}
   .city .el-input {width: 200px;}
   .modify .el-textarea__inner {width: 100%;resize: none;}
-  .next-btn {margin-left: calc( 50% - 160px );font-size: 18px;width: 320px;height: 50px;color: #fff;background-color: #4794fe;border-radius: 4px;border-top-style: none;border-right-style: none;border-bottom-style: none;border-left-style: none;}
+  .next-btn {font-size: 18px;width: 320px;height: 50px;color: #fff;background-color: #4794fe;border-radius: 4px;border-top-style: none;border-right-style: none;border-bottom-style: none;border-left-style: none;}
   .el-upload--picture-card {
     background-color: #fff; 
     border: 0; 
